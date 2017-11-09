a77ab9aec8732b2200b459fbe68ff1118812f306
Sebastian Almeida <jhoan.111@hotmail.com>
2017-11-08 20:57:07 -0500

    Se adicionan archivos

---
+++ CurrencyBitcoin/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  CurrencyBitcoin
+//
+//  Created by practica on 1/11/17.
+//  Copyright © 2017 SebasTitan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+
+    @IBOutlet var currentPicker: UIPickerView!
+    @IBOutlet var labelPrecio: UILabel!
+     
+    let currenciesArray = ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB"]
+    let currencyArray =
+        ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"
+        ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentPicker.delegate = self
+        currentPicker.dataSource = self
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currenciesArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currenciesArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(currenciesArray[row])
+    }
+}
+
